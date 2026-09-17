58e2780247b9cbead5f85beea45865dff9221533
Dr Code <nathanielnarth@gmail.com>
2026-09-18 00:56:41 +0100

    tag models

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category,Tag
 
 # ::: ADMIN CATEGORY
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name","slug","created_at","updated_at")
+    search_fields = ("name","slug")
+    prepopulated_fields = {"slug": ("name",)}
+
+# ::: ADMIN TAG
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     list_display = ("name","slug","created_at","updated_at")
     search_fields = ("name","slug")
     prepopulated_fields = {"slug": ("name",)}
--- blog/models.py
+++ blog/models.py
@@ -18,3 +18,20 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+# ::: TAGS
+class Tag(models.Model):
+
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    name = models.CharField(max_length=100,unique=True)
+    slug = models.SlugField(max_length=120,unique=True,db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
+    def __str__(self):
+        return self.name
